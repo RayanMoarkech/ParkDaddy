0b1282646cbe528b47dcdf02324e91909d546cac
Rayan Moarkech <rayanzone@hotmail.com>
2022-03-29 17:29:33 -0400

    Initial commit

---
+++ ParkDaddy/ParkDaddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkDaddyApp.swift
+//  ParkDaddy
+//
+//  Created by Rayan Moarkech on 2022-03-29.
+//
+
+import SwiftUI
+
+@main
+struct ParkDaddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ParkDaddy
+ 
