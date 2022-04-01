70eea24f133a394cdb64d25b7a6bc7552404f025
Lujain Khalaf <67845184+LujainKhalaf@users.noreply.github.com>
2022-04-01 11:56:54 -0400

    - Created Home Page - Added App Icon - Added Camera Permissions - Added AVCamera preview - Added Small App Tutorial Description

--- ParkDaddy/ContentView.swift
+++ ParkDaddy/ContentView.swift
@@ -6,16 +6,194 @@
 //
 
 import SwiftUI
+import UIKit
+import AVFoundation
 
-struct ContentView: View {
+struct StartPage: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack {
+                Image("ParkDaddy")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                Text("Your No.1 parking assistant")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                NavigationLink( destination: Tutorial_1(), label: {
+                    Text("Let's Get Started →")
+                        .padding()
+                        .background(Color(red: 0.999, green: 0.272, blue: 0.17))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .frame(maxHeight: 100, alignment: .bottom)
+                    
+                })
+                
+                .scaleEffect(1)
+                
+                
+            }
+        }
     }
 }
 
+struct Tutorial_1: View {
+    var body: some View {
+                        
+            VStack {
+                Image("Car")
+                    .resizable()
+                    .frame(width: 319, height: 225)
+                Text("Just The Right Spot")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                Text("Parking tickets are a thing of the past")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.231, green: 0.256, blue: 0.294)/*@END_MENU_TOKEN@*/)
+                    .padding()
+            
+                
+                NavigationLink( destination: Tutorial_2(), label: {
+                    Text("Next →")
+                        .padding()
+                        .background(Color(red: 0.999, green: 0.272, blue: 0.17))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .frame(maxHeight: 100, alignment: .bottom)
+                    
+                })
+                ProgressView(value: 0.25).tint(.red).frame(maxHeight: 100, alignment: .bottom)
+                
+        }
+    }
+}
+
+
+struct Tutorial_2: View {
+    var body: some View {
+
+            VStack {
+                Image("Map")
+                    .resizable()
+                    .frame(width: 319, height: 225)
+                Text("Quick Navigation")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                Text("All Montreal Area")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                
+                NavigationLink( destination: Tutorial_3(), label: {
+                    Text("Next →")
+                        .padding()
+                        .background(Color(red: 0.999, green: 0.272, blue: 0.17))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .frame(maxHeight: 100, alignment: .bottom)
+                    
+                })
+                ProgressView(value: 0.5).tint(.red).frame(maxHeight: 100, alignment: .bottom)
+                
+
+        }
+    }
+}
+
+struct Tutorial_3: View {
+    var body: some View {
+
+            VStack {
+                Image("Meter")
+                    .resizable()
+                    .frame(width: 319, height: 225)
+                Text("Easy Scanning")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                Text("Scan the parking signs and we'll do the rest")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                
+                NavigationLink( destination: HomePage(), label: {
+                    Text("Get Started")
+                        .padding()
+                        .background(Color(red: 0.999, green: 0.272, blue: 0.17))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .frame(maxHeight: 100, alignment: .bottom)
+                    
+                })
+                ProgressView(value: 1).tint(.red).frame(maxHeight: 100, alignment: .bottom)
+                
+
+        }
+    }
+}
+
+struct HomePage: View {
+    var body: some View {
+
+            VStack {
+                    Color(red: 0.999, green: 0.272, blue: 0.17).frame(height: 100, alignment: .top).ignoresSafeArea()
+
+                Spacer()
+                
+                
+                NavigationLink( destination: CameraView(), label: {
+                    Image("Park")
+                        .resizable()
+                        .frame(width: 302, height: 297)
+                    
+                }) . navigationBarBackButtonHidden(true)
+//                Button {
+//
+//                } label: {
+//                    Image("Park")
+//                        .resizable()
+//                        .frame(width: 302, height: 297)
+//                }
+               
+                Text("Tap to Start Scanning")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
+                    .padding()
+                
+                
+        }
+    }
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            StartPage()
+            Tutorial_1()
+            Tutorial_2()
+            Tutorial_3()
+            HomePage()
+            CameraView()
+            
+        }
     }
+}
 }
--- ParkDaddy/ParkDaddyApp.swift
+++ ParkDaddy/ParkDaddyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ParkDaddyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartPage()
         }
     }
 }
