a13aada42c67530648bfdd53a65f8a8e0786e8f7
Rayan Moarkech <rayanzone@hotmail.com>
2022-04-13 19:32:14 -0400

    Create CameraView.swift

---
+++ ParkDaddy/Views/CameraView.swift
@@ -0,0 +1,57 @@
+//
+//  CameraView.swift
+//  ParkDaddy
+//
+//  Created by Rayan Moarkech on 2022-04-13.
+//
+
+import SwiftUI
+import UIKit
+import AVFoundation
+
+struct CameraView: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UIImagePickerController
+    
+    
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        let viewController = UIViewControllerType()
+        viewController.delegate = context.coordinator
+        viewController.sourceType = .camera
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> CameraView.Coordinator {
+        return Coordinator(self)
+    }
+}
+
+extension CameraView {
+    class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraView
+        
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            print("Cancel pressed")
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            }
+        }
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
