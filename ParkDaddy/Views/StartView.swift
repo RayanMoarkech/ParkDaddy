603a22609717850ee649100527b9678782457615
Rayan Moarkech <rayanzone@hotmail.com>
2022-04-13 19:45:29 -0400

    cleanup

--- ParkDaddy/Views/StartView.swift
+++ ParkDaddy/Views/StartView.swift
@@ -10,30 +10,32 @@ import UIKit
 import AVFoundation
 
 struct StartView: View {
+    
     var body: some View {
-        NavigationView{
+        
+        NavigationView {
+            
             VStack {
+                
                 Image("ParkDaddy")
                     .resizable()
                     .frame(width: 300, height: 300)
+                
                 Text("Your No.1 parking assistant")
                     .font(.callout)
                     .fontWeight(.light)
                     .foregroundColor(Color(red: 0.231, green: 0.256, blue: 0.294))
                     .padding()
                 
-                NavigationLink( destination: FirstTutorialView(), label: {
+                NavigationLink(destination: FirstTutorialView(), label: {
                     Text("Let's Get Started →")
                         .padding()
                         .background(Color(red: 0.999, green: 0.272, blue: 0.17))
                         .foregroundColor(Color.white)
                         .cornerRadius(10)
                         .frame(maxHeight: 100, alignment: .bottom)
-                    
                 })
-                
                 .scaleEffect(1)
-                
                 
             }
         }
