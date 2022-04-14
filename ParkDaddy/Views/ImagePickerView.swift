38d849fefd8e38222e7400585e0d61621c9db4fa
Rayan Moarkech <rayanzone@hotmail.com>
2022-04-13 21:20:30 -0400

    Create ImagePickerView.swift

---
+++ ParkDaddy/Views/ImagePickerView.swift
@@ -0,0 +1,47 @@
+//
+//  ImagePickerView.swift
+//  ParkDaddy
+//
+//  Created by Rayan Moarkech on 2022-04-13.
+//
+
+import SwiftUI
+import UIKit
+import AVFoundation
+
+struct ImagePickerView: UIViewControllerRepresentable {
+
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+        
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+    
+    // Connecting the Coordinator class with this struct
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
